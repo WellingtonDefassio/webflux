3b2ec5a8924c52e98e4791ec37efd3b7365ba435
Wellington Defassio <89281805+WellingtonDefassio@users.noreply.github.com>
2023-06-11 14:31:39 -0300

    create controller interface and request/response objects

---
+++ src/main/java/wdefassio/io/webflux/model/request/UserRequest.java
@@ -0,0 +1,4 @@
+package wdefassio.io.webflux.model.request;
+
+public record UserRequest(String name, String password, String email) {
+}
---
+++ src/main/java/wdefassio/io/webflux/model/response/UserResponse.java
@@ -0,0 +1,4 @@
+package wdefassio.io.webflux.model.response;
+
+public record UserResponse(String id, String name, String email, String password) {
+}

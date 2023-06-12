51ef1c2bb38fe214df08b17fe0e6d6f480578f00
Wellington Defassio <89281805+WellingtonDefassio@users.noreply.github.com>
2023-06-12 00:02:41 -0300

    integration test delete/update

--- src/test/java/wdefassio/io/webflux/controller/UserControllerImplTest.java
+++ src/test/java/wdefassio/io/webflux/controller/UserControllerImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import wdefassio.io.webflux.entity.User;
 import wdefassio.io.webflux.mapper.UserMapper;
@@ -96,14 +97,55 @@ class UserControllerImplTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("Test find all endpoint with success")
+    void testFindAllWithSuccess() {
+
+        String id = "123456id";
+        UserResponse userResponse = new UserResponse(id, "Valdir", "valdir@email.com", "123456");
+        when(userService.findAll()).thenReturn(Flux.just(User.builder().build()));
+        when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+        client.get().uri(baseUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.[0].id").isEqualTo(id);
+
+
     }
 
     @Test
+    @DisplayName("Test update")
     void update() {
+        String id = "123456id";
+        UserResponse userResponse = new UserResponse(id, "Valdir", "valdir@email.com", "123456");
+        UserRequest request = new UserRequest(" valdir ", "123456", "valdir@email.com");
+
+        when(userService.update(anyString(), any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
+        when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+        client.patch().uri(baseUri + "/" + id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(request))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(id);
+
+
     }
 
     @Test
-    void delete() {
+    @DisplayName("delete test")
+    void deleteTest() {
+
+        String id = "123456id";
+        when(userService.delete(anyString())).thenReturn(Mono.just(User.builder().build()));
+
+        client.delete().uri(baseUri + "/" + id)
+                .exchange()
+                .expectStatus().isOk();
+
     }
 }

95fac8b5f57ad769e923400518ec90b70394b0fe
Wellington Defassio <89281805+WellingtonDefassio@users.noreply.github.com>
2023-06-11 21:50:11 -0300

    update implementation

--- src/main/java/wdefassio/io/webflux/mapper/UserMapper.java
+++ src/main/java/wdefassio/io/webflux/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package wdefassio.io.webflux.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import wdefassio.io.webflux.entity.User;
 import wdefassio.io.webflux.model.request.UserRequest;
 import wdefassio.io.webflux.model.response.UserResponse;
@@ -13,7 +14,10 @@ import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest userRequest);
+    @Mapping(target = "id", ignore = true)
+    User toEntity(UserRequest request, @MappingTarget User entity);
 
     UserResponse toResponse(final User user);
+
 
 }
